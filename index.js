de5a93051730ef1ed9df9ccb698acd2a270dd281
cfrancisco726 <cfrancisco726@gmail.com>
2018-03-08 00:47:40 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'There' });
+});
+// route handler
+
+const PORT = process.env.PORT || 5000
+// environment variables that are set in the underlying runtime that node is running on top off
+// herokus oppportunity to send us runtime configuration after we deploy
+// if heroku is running our app in production then we can freely use process.env.PORT
+// if in dev env like on our machines then that variable might not be defined
+// 5000 for dev
+
+app.listen(PORT);
