bda96b321da01a3b320b3993ee615c133cdaf6bb
cfrancisco726 <cfrancisco726@gmail.com>
2018-04-03 18:28:39 -0400

    test prod

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -12,9 +12,11 @@ const Survey = mongoose.model('surveys');
 // mongoose model class
 module.exports = app => {
 	app.get('/api/surveys', requireLogin, async (req, res) => {
-		const surveys = await Survey.find({ _user: req.user.id }).select({ recipients: false })
+		const surveys = await Survey.find({ _user: req.user.id }).select({
+			recipients: false
+		});
 
-		res.send(surveys)
+		res.send(surveys);
 	});
 
 	app.get('/api/surveys/:surveyId/:choice', (req, res) => {
