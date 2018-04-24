5d1d550638fcc49f7520094fd58b63ab1b0740bd
cfrancisco726 <cfrancisco726@gmail.com>
2018-04-24 18:19:39 -0400

    first commit

---
+++ config/dev.js
@@ -0,0 +1,14 @@
+// dev.js - don't commit this
+module.exports = {
+	googleClientID:
+		'375293511494-n4g5uf53ehdbsg64fgj5on550824pvdi.apps.googleusercontent.com',
+	googleClientSecret: 'BMqdA5RMx5BX2dqebPrtAQ_i',
+	mongoURI:
+		'mongodb://cfrancisco726:password@ds163918.mlab.com:63918/email-dev',
+	cookieKey: 'akjfalkdjsfaalksdjf',
+	stripePublishableKey: 'pk_test_MP1dunfp4Jo9KQbkgEfhCytU',
+	stripeSecretKey: 'sk_test_0ooHfIVa2qm93H8qrxZ08Wr0',
+	sendGridKey:
+		'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',
+	redirectDomain: 'http://localhost:3000'
+};
